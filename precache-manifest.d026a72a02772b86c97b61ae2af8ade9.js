0f757f1f2f6a00f7c44e35bce53eee6da753c280
Aditya Kumar <adshin21@gmail.com>
2020-09-24 14:50:09 +0530

    Updates

---
+++ precache-manifest.d026a72a02772b86c97b61ae2af8ade9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21659dc18f8b1321e0a9f269be3d6e8e",
+    "url": "/order-dashboard/index.html"
+  },
+  {
+    "revision": "dd2c621b71485ed58242",
+    "url": "/order-dashboard/static/css/main.88824ec2.chunk.css"
+  },
+  {
+    "revision": "64af3d30b800bf786ae8",
+    "url": "/order-dashboard/static/js/2.dabe5010.chunk.js"
+  },
+  {
+    "revision": "7e847b9f7848868efb563268f6e7c7d4",
+    "url": "/order-dashboard/static/js/2.dabe5010.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd2c621b71485ed58242",
+    "url": "/order-dashboard/static/js/main.202fb31b.chunk.js"
+  },
+  {
+    "revision": "d5cb302b5da94dd16ba1",
+    "url": "/order-dashboard/static/js/runtime-main.944a61d6.js"
+  }
+]);
